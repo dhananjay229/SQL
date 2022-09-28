14b316ea3c85c2ce28f5bc2364d9e7e6c29e9a6b
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-09-28 21:13:04 +0530

    Update and rename Select.sql to 1_SELECT.sql

---
+++ sql SELECT statement/1_SELECT.sql
@@ -0,0 +1,8 @@
+--select all colume from table 
+
+SELECT * FROM Employees;
+
+
+--select employee id and employee name
+
+SELECT employee_id,emp_name FROM Employees;
--- sql SELECT statement/Select.sql
+++
@@ -1,6 +0,0 @@
-//select all colume from table 
-SELECT * FROM Employees;
-
-
-//select employee id and employee name
-SELECT employee_id,emp_name FROM Employees;
