db1442a42e6cb65f060928bfc29ec4f73dc18957
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-10-01 10:36:32 +0530

    Add files via upload

---
+++ sql SELECT statement/11_Select-Distinct.sql
@@ -0,0 +1,6 @@
+-- select all distinct different data values
+
+SELECT DISTINCT
+	gender
+FROM
+	employees;
---
+++ sql SELECT statement/12_Order-By.sql
@@ -0,0 +1,43 @@
+SELECT
+	*
+FROM
+	employees
+ORDER BY
+	first_name;
+    
+-- ASC - Ascending
+
+SELECT
+	*
+FROM
+	employees
+ORDER BY
+	first_name ASC;
+    
+-- DESC - Descending
+
+SELECT
+	*
+FROM
+	employees
+ORDER BY
+	first_name DESC;
+    
+    
+-- order by work on iteger as well
+
+SELECT
+	*
+FROM
+	employees
+ORDER BY
+	emp_no DESC;
+    
+-- ASC 
+
+SELECT
+	*
+FROM
+	employees
+ORDER BY
+	emp_no ASC;
