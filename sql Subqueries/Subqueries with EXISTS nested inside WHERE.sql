71efa7685d815b557374b80ce68296e25e75e0a6
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-10-19 12:01:12 +0530

    Add files via upload

---
+++ sql Subqueries/Subqueries with EXISTS nested inside WHERE.sql
@@ -0,0 +1,28 @@
+-- EXISTS -> checks certain row values are found within a subquery
+
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no);
+            
+            
+
+SELECT 
+    e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+    EXISTS( SELECT 
+            *
+        FROM
+            dept_manager dm
+        WHERE
+            dm.emp_no = e.emp_no
+        ORDER BY emp_no);
